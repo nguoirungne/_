6e5606e0ed649c84dfc08039bb94026fe4fe2a3a
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-13 01:58:03 +0700

    Add files via upload

--- w/eht/l.lua
+++ w/eht/l.lua
@@ -1,1 +1,29 @@
 
+NR.v.eht={
+   package='com.superplanet.evilhunter',
+   check={
+      date='241212',
+      luaName='test.lua'
+   }
+   note={
+      '📢 eht note!'
+   }
+   class={
+      dataAS={'AdminShopData', 8027758069468193857, '卮潨䑰瑡', 4},
+      dataI={'ItemData', 7022344665498481737, '瑉浥慄慴', 0}
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
