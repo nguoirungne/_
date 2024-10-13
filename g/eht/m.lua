45939761c559e418476395da4d6b1b28d8de99c8
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-14 01:04:29 +0700

    Add files via upload

---
+++ g/eht/m.lua
@@ -0,0 +1,31 @@
+function mainM()
+   gg.setVisible(''>'')
+   while true do
+      local _1=gg.choice(eht.menu.merge, nil, 'MENU')
+      if not _1 then NR.f.waitM() end
+      if _1==#eht.menu.merge then NR.f.exitM() end
+      if _1==1 then end
+      if _1==2 then end
+   end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
