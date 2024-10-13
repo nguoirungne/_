9fa6155cc45a852accf28c1ea29cd2f27d211ee5
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-13 14:36:57 +0700

    Add files via upload

--- g/eht/l.lua
+++ g/eht/l.lua
@@ -1,1 +1,40 @@
+eht={}
+
+eht.package='com.superplanet.evilhunter'
+
+eht.check={
+   oDate='241212',
+   sName='NEW_SCRIPT.lua',
+   dPW='NR',
+   dBool={sn=false,od=false,pw=false}
+}
+
+eht.note={
+   '📢 test eht note!\n'
+}
+
+eht.class={
+   dataAS={'AdminShopData', 8027758069468193857, '卮潨䑰瑡', 4},
+   dataI={'ItemData', 7022344665498481737, '瑉浥慄慴', 0}
+}
+   
+eht.checkUpdate=function()
+   NR.f.checkLName(eht.check.sName)
+   NR.f.checkDate(eht.check.oDate)
+   return true
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
