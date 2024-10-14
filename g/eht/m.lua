8fae1deffa16182f538f36cafbe32a678113236a
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-14 15:39:11 +0700

    Add files via upload

--- g/eht/m.lua
+++ g/eht/m.lua
@@ -1,9 +1,9 @@
 function mainM()
    gg.setVisible(''>'')
    while true do
-      local _1=gg.choice(eht.mergeM(eht.menu), nil, 'EHT MENU')
+      local _1=gg.choice(eht.mergeM(eht.mainM), nil, 'EHT MENU')
       if not _1 then NR.f.waitM() end
-      if _1==#eht.menu.merge then NR.f.exitM()
+      if _1==#eht.mainM.merge then NR.f.exitM()
       else checkF(_1) end
    end
 end
@@ -12,7 +12,7 @@ function userM()
    while true do
       local _1=gg.choice(eht.mergeM(eht.userM), nil, 'MENU')
       if not _1 then NR.f.waitM() end
-      if _1==#eht.menu.merge then mainM()
+      if _1==#eht.userM.merge then mainM()
       else userB(_1) end      
    end
 end
@@ -21,7 +21,7 @@ function hunterM()
    while true do
       local _1=gg.choice(eht.mergeM(eht.hunterM), nil, 'MENU')
       if not _1 then NR.f.waitM() end
-      if _1==#eht.menu.merge then mainM()
+      if _1==#eht.hunterM.merge then mainM()
       else hunterB(_1) end
    end
 end
@@ -30,7 +30,7 @@ function shopM()
    while true do
       local _1=gg.choice(eht.mergeM(eht.shopM), nil, 'MENU')
       if not _1 then NR.f.waitM() end
-      if _1==#eht.menu.merge then mainM()
+      if _1==#eht.shopM.merge then mainM()
       else shopB(_1) end
    end
 end
