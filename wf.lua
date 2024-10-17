6d7715a377609865fe0a405fa4d735251ff1b396
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-18 02:51:06 +0700

    Add files via upload

---
+++ wf.lua
@@ -0,0 +1,229 @@
+NR.v.link={
+   teleUZ='Https://t.me/undeadzone',
+   teleNR='Https://t.me/nguoirungne'	
+}
+NR.v.note={
+   '📢 Https://t.me/undeadzone!\n'	
+}
+NR.v.blockC={
+   server={'https://cdn.now.gg', 'https://404.playrix.com'},
+   cSuite={'TLS_AES_256_GCM_SHA384'}
+}
+
+NR.f.setXLogs=function(__)
+   local _1={}
+   local _2=string.rep(' ',1048576)
+   for _=1,1024 do
+      _1[_]=_2
+   end
+   for _=1,__ do
+      pcall(gg.alert, _1)
+   end
+end --NR.f.setXLogs
+
+NR.f.antiLoad=function()
+   for _=1,4 do 
+      for ___=1,math.random(1000,1100) do 
+         local _1=string.rep(' ',math.random(500,5000))
+         _G[NR.f.l('hke`')](_1)
+      end 
+   end
+end --NR.f.antiLoad
+
+NR.f.num2Hex=function(__) return string.format('%02x',__) end --NR.f.num2Hex
+
+NR.f.antiCapture=function()
+   local _1=gg.makeRequest(NR.v.blockC.server[math.random(2)])
+   local _2='× Connection error!'
+   if not _1 then gg.alert(_2) os.exit() end
+   if (NR.v.blockC.cSuite[1]==_1.cipherSuite) then
+      return true 
+   else
+      gg.alert(_2)
+      os.exit()
+   end                                    
+end --NR.f.antiCapture
+
+NR.f.antiLook=function(__)
+   if (gg.isVisible()) then
+      gg.setVisible(false)
+      gg.alert('× Don\'t Look')
+      NR.f.antiLog(__) return true
+   end return false
+end --NR.f.antiLook
+
+NR.f.antiLog=function(__)
+   local _1=string.rep('[script from t.me/undeadzone]',1000000)
+   local _2=3
+   gg.makeRequest(_1)
+   while (_2>0) do
+      _2=_2-1
+      gg.setVisible(false)
+      for i=1,__ do
+         gg.toast('loading '.._2..'.'..__-i)
+         gg.makeRequest(_1)
+         debug.traceback(1,nil,_1)
+         NR.f.antiLook(3)
+      end
+      if (_2==0) then gg.toast('√ Complete!') end
+   end
+end --NR.f.antiLog
+
+NR.f.checkPW=function(__)
+   if (__.dBool.pw) then return true else
+      local _1=gg.prompt({'Enter Password:'},{__.dPW},{'text'})
+      if not _1 then gg.toast('Canceled!') return end
+      if (_1[1]==NR.v.dAD) then gg.toast('√ Logged in by admin!') __.dBool.pw=true __.dBool.ad=true __.oDate='99999999' return true end
+      if (_1[1]==NR.v.dTS) then gg.toast('√ Logged in by tester!') __.dBool.pw=true __.dBool.ts=true return true end
+      if (_1[1]==__.dPW) then gg.toast('√ Logged in by user!') __.dBool.pw=true return true end
+      gg.toast('× Password does not match!') 
+      return false
+   end
+end --NR.f.checkPW
+
+NR.f.makePW=function(__)
+   local _1=NR.f.checkDate()
+   local _2=string.sub(_1,7,8)
+   return 'NR'..(_1*_2/__)
+end --NR.f.makePW
+
+NR.f.checkDate=function(__)
+   local _1={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+   local _2=gg.makeRequest(NR.v.blockC.server[2]).headers.Date[1]
+   for _,___ in ipairs(_1) do
+      if (___==string.sub(_2, 9, 11)) then
+         local __1=string.sub(_2, 13, 16)..string.format('%02s',_)..string.sub(_2, 6, 7)
+         if (__==nil) or (__.dBool.ad) then return __1 else
+            if (__.oDate<__1) then
+               gg.alert('× This script has expired.') os.exit()
+            else return __1 end
+         end
+      end
+   end  
+end --NR.f.checkDate
+
+NR.f.scanRand=function()
+   gg.setVisible(false)
+   gg.clearResults()
+   local _1=math.random(1000000000,2100000000)
+   local _2={1,2,4,8,16,32,64,127}
+   local _3={1,2,4,8,16,32,64,16384,32768,65536,131072,262144,524288,1048576,-2080896}      
+   gg.setRanges(_3[math.random(1,#_3)])
+   gg.searchNumber('x'.._G.NR.f.num2Hex(_1),_2[math.random(1,#_2)])
+   gg.refineAddress(0,-1,_2[math.random(1,#_2)])
+   gg.refineNumber(0,_2[math.random(1,#_2)])
+   gg.getResults(gg.getResultsCount())
+   gg.clearResults()
+end --NR.f.scanRand
+
+NR.f.checkLName=function(__)
+   if (NR.v.lName==__) then return true else
+      gg.alert('× Do not change the script name!')
+      gg.copyText(__)
+      gg.alert('√ Script name has copied!')
+      NR.f.exitM()  
+   end       
+end --NR.f.checkLName
+
+NR.f.menuTab=function(__)
+   local _1={}
+   for _=1, #__[1] do _1[_]=__[1][_][1] end
+   local _2=gg.choice(_1, nil, __.title)
+   if not _2 then NR.f.waitM() end
+   return _2 and __[1][_2][2](__[1][_2][3])
+end --NR.f.menuTab
+
+NR.f.waitM=function()
+   gg.setVisible(false)
+   while true do --NR.f.antiCapture() do
+      if (gg.isVisible()) then
+         gg.setVisible(false)
+         NR.f.scanRand()
+      break else
+         NR.f.scanRand()
+      end
+      gg.sleep(1000)
+   end
+end --NR.f.waitM
+
+NR.f.exitM=function()
+   gg.clearList()
+   gg.clearResults()
+   gg.alert('[NR] See you later!', NR.v.link.teleUZ)
+   gg.setVisible(true)
+   os.exit()
+end --NR.f.exitM
+
+_G[NR.f.l('LP')][NR.f.l('w')][NR.f.l('gBG')]=_G[NR.f.l('LP')][NR.f.l('g')][NR.f.l('kgmcVQ')](2212)
+_G[NR.f.l('LP')][NR.f.l('w')][NR.f.l('sWP')]=_G[NR.f.l('LP')][NR.f.l('g')][NR.f.l('kgmcVQ')](2024)..NR.f.l('PAWP')
+
+NR.f.getB16LE=function(...)
+   local _0={...}
+   gg.clearResults()
+   gg.setVisible(false)
+   gg.setRanges(32)
+   gg.searchNumber(';'.._0[1][3], 1)
+   gg.refineAddress(_0[1][4], -1)
+   local _1=gg.bytes(_0[1][1])
+   gg.refineNumber(_1[_0[1][4]+2]*256+_1[_0[1][4]+1], 2)
+   local _1=gg.getResults(gg.getResultsCount())
+   gg.clearResults()
+   for _,__ in ipairs(_1) do
+      local __1={{address=__.address-_0[1][4], flags=32}}
+      local __2={{address=__1[1].address+#_0[1][1], flags=1}}
+      local __3={{address=__1[1].address-0x24, flags=4}}
+      local __4={gg.getValues(__1)[1].value, gg.getValues(__2)[1].value, gg.getValues(__3)[1].value}
+      if (__4[1]==_0[1][2]) and (__4[2]==0) and (__4[3]==73) then
+         local __5={{address=__1[1].address-8, flags=32}}
+         __5[1].address=(gg.getValues(__5)[1].value)+8
+         _0[1][5]=gg.getValues(__5)[1].value
+         return _0[1][5]
+      end
+   end
+end --NR.f.getB16LE
+
+NR.f.setScan=function(...)
+   local _0={...}
+   if (_0[1]~=nil) then gg.setRanges(_0[1]) end
+   if (_0[2]~=nil) then gg.setVisible(_0[2]) end
+   gg.clearResults()
+end --NR.f.setScan
+
+NR.f.allResults=function() return gg.getResults(gg.getResultsCount()) end --NR.f.allResults
+
+NR.f.scan=function(...)
+   local _0={...}
+   gg.searchNumber(_0[1], _0[2])
+   if (_0[3]~=nil) then gg.refineNumber(_0[3], _0[2]) end
+   if (_0[4]~=nil) then gg.refineAddress(_0[4], -1) end
+end --NR.f.scan
+
+NR.f.copyItems=function(...)
+   local _0={...}
+   local _1={{}}
+   _1[1].address=_0[1]
+   _1[1].flags=_0[2]
+   if (_0[3]~=nil) then _1[1].value=_0[3] else
+      _1[1].value=gg.getValues(_1)[1].value
+   end
+   if (_0[4]~=nil) then _1[1].name=_0[4] end
+   if (_0[5]~=nil) then _1[1].freeze=_0[5] end
+   if (_0[6]~=nil) then _1[1].freezeType=_0[6] end
+   if (_0[7]~=nil) then _1[1].freezeFrom=_0[7] end
+   if (_0[8]~=nil) then _1[1].freezeTo=_0[8] end
+   if (_0[9]==true) then gg.setValues(_1) end
+   if (_0[10]==true) then gg.addListItems(_1) end
+   return _1
+end --NR.f.copyItems
+
+
+
+
+
+
+
+
+
+
+
+
