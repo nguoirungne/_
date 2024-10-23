173ccbe079b1ee76ad2f3ca6c1d70eab5096ad6a
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-23 19:50:18 +0700

    Add files via upload

--- g/eht/gf.lua
+++ g/eht/gf.lua
@@ -468,16 +468,12 @@ function postGLevel()
    end
 end
 --###################################
-function gearOptions()
-   gg.alert('gearOptions Updated 4')
-   local _0=true
-   local _1={eht.var.dataG.descT[10],eht.var.dataG.descT[11], eht.var.dataG.descT[12], eht.var.dataG.descT[13], eht.var.dataG.descT[14], eht.var.dataG.descT[15], eht.var.dataG.descT[16], eht.var.dataG.descT[17]}
-   local _2={eht.var.dataG.descT[18],eht.var.dataG.descT[19],eht.var.dataG.descT[20],eht.var.dataG.descT[21],eht.var.dataG.descT[22]}
-   local _3={'B','A','S','SS','SSS','UNIQ'}
-   local _4_2={' > ',' > ',' > ',' > ',' > '}
-   local _4={eht.var.dataG.descT[1].._4_2[1], eht.var.dataG.descT[3].._4_2[2], eht.var.dataG.descT[4].._4_2[3], eht.var.dataG.descT[6].._4_2[4], eht.var.dataG.descT[8]..' #1'.._4_2[5], eht.paidM.desc[#eht.paidM.desc]}
-   
-end
+
+
+
+
+
+
 --###################################
 --###################################
 function itemB()
