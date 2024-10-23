94ab13e3375dafe8b0113c08f9bcbea904dfaf4f
NR <161801786+nguoirungne@users.noreply.github.com>
2024-10-23 19:25:11 +0700

    Add files via upload

--- g/eht/gf.lua
+++ g/eht/gf.lua
@@ -469,51 +469,32 @@ function postGLevel()
 end
 --###################################
 function gearOptions()
-   gg.alert('gearOptions Updated 1')
-   
+   gg.alert('gearOptions Updated 3')
+   local _0=true
    local _1={eht.var.dataG.descT[10],eht.var.dataG.descT[11], eht.var.dataG.descT[12], eht.var.dataG.descT[13], eht.var.dataG.descT[14], eht.var.dataG.descT[15], eht.var.dataG.descT[16], eht.var.dataG.descT[17]}
    local _2={eht.var.dataG.descT[18],eht.var.dataG.descT[19],eht.var.dataG.descT[20],eht.var.dataG.descT[21],eht.var.dataG.descT[22]}
    local _3={'B','A','S','SS','SSS','UNIQ'}
-   ::startsetup::
    local _4_2={' > ',' > ',' > ',' > ',' > '}
-   local _4__={}
    local _4={eht.var.dataG.descT[1].._4_2[1], eht.var.dataG.descT[3].._4_2[2], eht.var.dataG.descT[4].._4_2[3], eht.var.dataG.descT[6].._4_2[4], eht.var.dataG.descT[8]..' #1'.._4_2[5], eht.paidM.desc[#eht.paidM.desc]}
-   local _5=gg.choice(_4, nil, eht.var.dataG.descT[27])
-      if not _5 then goto waituser end
-      if (_5==1) then goto setgeartype end
-      if (_5==2) then goto setquality end
-      if (_5==3) then goto setlevel end
-      if (_5==4) then goto setoptionvalue end
-      if (_5==5) then goto donesetup end
-      if (_5==6) then gg.toast(NR.v.link.teleUZ) mainM() end
-   ::setgeartype::
-   local _6=gg.choice(_1, nil, eht.var.dataG.descT[1])
-      if not _6 then goto startsetup end
-      if (_6==1) then _4_2[1]=' > '.._1[1] _4__[1]=0 goto startsetup end
-      if (_6==2) then _4_2[1]=' > '.._1[2] _4__[1]=1 goto startsetup end
-      if (_6==3) then _4_2[1]=' > '.._1[3] _4__[1]=2 goto startsetup end
-      if (_6==4) then _4_2[1]=' > '.._1[4] _4__[1]=3 goto startsetup end
-      if (_6==5) then _4_2[1]=' > '.._1[5] _4__[1]=4 goto startsetup end
-      if (_6==6) then _4_2[1]=' > '.._1[6] _4__[1]=5 goto startsetup end
-      if (_6==7) then _4_2[1]=' > '.._1[7] _4__[1]=6 goto startsetup end
-      if (_6==8) then _4_2[1]=' > '.._1[8] _4__[1]=7 goto startsetup end
-   ::setquality::
-   local _7=gg.choice(_2, nil, eht.var.dataG.descT[3])
-      if not _7 then goto startsetup end
-      if (_7==1) then _4_2[2]=' > '.._2[1] _4__[2]=0 goto startsetup end
-      if (_7==2) then _4_2[2]=' > '.._2[2] _4__[2]=1 goto startsetup end
-      if (_7==3) then _4_2[2]=' > '.._2[3] _4__[2]=2 goto startsetup end
-      if (_7==4) then _4_2[2]=' > '.._2[4] _4__[2]=3 goto startsetup end
-      if (_7==5) then _4_2[2]=' > '.._2[5] _4__[2]=4 goto startsetup end
-   ::setlevel::
-   ::setoptionvalue::
-   ::waituser::
-   while (gg.isVisible()==false) do
-      gg.setVisible(false)
+   local _0_w=function()
+      while gg.isVisible()==false then
+         gg.setVisible(false)
+         gg.sleep(1000)
+      end
    end
-   goto startsetup
-   ::donesetup::
-   
+   local _0_s=function()
+      while _0 do
+         local __1=gg.choice(_4, nil, eht.var.dataG.descT[27])
+            if not __1 then _0_w() end
+            if (__1==1) then  end
+            if (__1==2) then  end
+            if (__1==3) then  end
+            if (__1==4) then  end
+            if (__1==5) then  end
+            if (__1==6) then gg.toast(NR.v.link.teleUZ) mainM() end
+      end --while
+   end
+   _0_s()
 end
 --###################################
 --###################################
